ef7737f6f0328e3b5b77d322085438b0d7fa7aeb
Phomai <nguyentinhoaian@gmail.com>
2024-07-02 22:41:30 +0700

    commit env

---
+++ Server/config/MongoDb.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDatabase = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+export default connectDatabase;
